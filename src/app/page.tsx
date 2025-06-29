2ff8f73136cb0aa90178024d75e8b621f38d51f0
Shamil <shamil.300085@gmail.com>
2025-06-28 18:43:10 -0700

    doc: added README

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,10 +21,10 @@ export default function Home() {
       {/* Main Section */}
       <section className="mx-auto max-w-250 p-6">
         <h1>Hello!</h1>
-        <p className="text-lg leading-relaxed">
-          This is a secure file exchange platform. Here you can upload any file (Max 50MB), 
-          it will be encrypted and stored in database and encryption key will be provided for you for temporary download. 
-          By giving the key you will get access to download the stored file.
+          <p className="text-lg leading-relaxed">
+            This is a secure file exchange platform. Here you can upload any file (Max 50MB), 
+            it will be encrypted and stored in database and encryption key will be provided for you for temporary download. 
+            By giving the key you will get access to download the stored file.
           </p>
       </section>
 
